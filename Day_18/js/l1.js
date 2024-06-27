5f4fb9523756a9ee77d976e40be361cfff9a6087
ShanDev <shanchen611@gmail.com>
2024-06-27 18:34:46 +0700

    fix: fix điều kiện bài 1

--- Day_18/js/l1.js
+++ Day_18/js/l1.js
@@ -1,4 +1,8 @@
 function fibonacci(n, a = 0, b = 1, count = 1) {
+  if (n < 0) {
+    console.log("Vui lòng nhập số nguyên dương.");
+    return;
+  }
   if (count <= n) {
     console.log(a);
     fibonacci(n, b, a + b, count + 1);
