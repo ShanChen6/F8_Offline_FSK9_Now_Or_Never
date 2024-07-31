11ba7c72d6148da7e10fc1e9d034a4932ac9149e
ShanDev <shanchen611@gmail.com>
2024-07-31 22:21:15 +0700

    update btvn day 29

--- Day_29/js/app.js
+++ Day_29/js/app.js
@@ -15,7 +15,36 @@ next.onclick = function () {
   }
   reloadSlider();
 };
+
+prev.onclick = function () {
+  if ((active = 1 < 0)) {
+    active = lengthItem;
+  } else {
+    active = active - 1;
+  }
+  reloadSlider();
+};
+
+var refreshSlider = setInterval(() => {
+  next.click();
+}, 5000);
+
 function reloadSlider() {
   var checkLeft = items[active].offsetLeft;
   list.style.left = -checkLeft + "px";
+
+  var lastActiveDot = document.querySelector(".slider .dots li.active");
+  lastActiveDot.classList.remove("active");
+  dots[active].classList.add("active");
+  clearInterval(refreshSlider);
+  refreshSlider = setInterval(() => {
+    next.click();
+  }, 5000);
 }
+
+dots.forEach((li, key) => {
+  li.addEventListener("click", function () {
+    active = key;
+    reloadSlider();
+  });
+});
